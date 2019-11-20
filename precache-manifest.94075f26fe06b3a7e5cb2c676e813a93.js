e7bfb8475cf59ebed1ee066c58ee2d448573d783
jaywcjlove <jaywcjlove@users.noreply.github.com>
2019-11-20 14:40:20 +0000

    Automated deployment: Wed Nov 20 14:40:20 UTC 2019 363b7576c70896ca6113d8d6111c1062c74ed83b

---
+++ precache-manifest.94075f26fe06b3a7e5cb2c676e813a93.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4992445256a5c160189a2aab1b2aae02",
+    "url": "/react-github-corners/index.html"
+  },
+  {
+    "revision": "4e7df7e746bbf28cb8ca",
+    "url": "/react-github-corners/static/css/main.e0e2716c.css"
+  },
+  {
+    "revision": "4e7df7e746bbf28cb8ca",
+    "url": "/react-github-corners/static/js/main.293b576e.js"
+  },
+  {
+    "revision": "49f98c8e3027a1c3b2b7a0d65b84a6a5",
+    "url": "/react-github-corners/static/media/logo.49f98c8e.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-github-corners/precache-manifest.94075f26fe06b3a7e5cb2c676e813a93.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-github-corners/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
