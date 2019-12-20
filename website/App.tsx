c6383bb82c9a672d198c9f3a762a6d1b6299b1eb
jaywcjlove <398188662@qq.com>
2019-12-20 10:42:40 +0800

    doc: Modify website style.

--- website/App.tsx
+++ website/App.tsx
@@ -16,6 +16,8 @@ const App: React.FC = () => {
         bgColor={bgColor}
         color={color}
         position={position}
+        fixed
+        zIndex={99}
         target="__blank"
         href="https://github.com/uiwjs/react-github-corners"
       />
