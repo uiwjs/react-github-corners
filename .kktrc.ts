49a8d929c62cd778b77f3623200edf7c61676d0a
jaywcjlove <398188662@qq.com>
2022-03-28 15:32:28 +0800

    feat: support bundle feature.

--- .kktrc.ts
+++ .kktrc.ts
@@ -6,43 +6,55 @@ import scopePluginOptions from '@kkt/scope-plugin-options';
 import pkg from './package.json';
 
 export default (conf: Configuration, env: 'production' | 'development', options: LoaderConfOptions) => {
-  conf = rawModules(conf, env, { ...options });
-  conf = scopePluginOptions(conf, env, {
-    ...options,
-    allowedFiles: [
-      path.resolve(process.cwd(), 'README.md')
-    ]
-  });
-  // Get the project version.
-  conf.plugins!.push(new webpack.DefinePlugin({
-    VERSION: JSON.stringify(pkg.version),
-  }));
-  if (env === 'production') {
-    conf.output = { ...conf.output, publicPath: './' };
-    conf.optimization = {
-      ...conf.optimization,
-      splitChunks: {
-        cacheGroups: {
-          reactvendor: {
-            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-            name: 'react-vendor',
-            chunks: 'all',
-          },
-          refractor: {
-            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
-            name: 'refractor-prismjs-vendor',
-            chunks: 'all',
-          },
-          runtime: {
-            test: /[\\/]node_modules[\\/](@babel)[\\/]/,
-            name: 'babel-vendor',
-            chunks: 'all',
-          },
-          parse5: {
-            test: /[\\/]node_modules[\\/](parse5)[\\/]/,
-            name: 'parse5-vendor',
-            chunks: 'all',
-          },
+  if (options.bundle) {
+    conf.output!.library = '@uiw/react-github-corners';
+    conf.externals = {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+    };
+  } else {
+    conf = rawModules(conf, env, { ...options });
+    conf = scopePluginOptions(conf, env, {
+      ...options,
+      allowedFiles: [
+        path.resolve(process.cwd(), 'README.md')
+      ]
+    });
+    // Get the project version.
+    conf.plugins!.push(new webpack.DefinePlugin({
+      VERSION: JSON.stringify(pkg.version),
+    }));
+    if (env === 'production') {
+      conf.output = { ...conf.output, publicPath: './' };
+      conf.optimization = {
+        ...conf.optimization,
+        splitChunks: {
+          cacheGroups: {
+            reactvendor: {
+              test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+              name: 'react-vendor',
+              chunks: 'all',
+            },
+            refractor: {
+              test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+              name: 'refractor-prismjs-vendor',
+              chunks: 'all',
+            },
+            runtime: {
+              test: /[\\/]node_modules[\\/](@babel)[\\/]/,
+              name: 'babel-vendor',
+              chunks: 'all',
+            },
+            parse5: {
+              test: /[\\/]node_modules[\\/](parse5)[\\/]/,
+              name: 'parse5-vendor',
+              chunks: 'all',
+            },
+          }
         }
       }
     }
